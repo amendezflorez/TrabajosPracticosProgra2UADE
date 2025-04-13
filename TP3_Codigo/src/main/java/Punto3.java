5ab92eba1d0f3b37d90603855bc722f426c8ad78
Lestergil7 <lestergildiaz@gmail.com>
2025-04-12 21:24:57 -0300

    Punto 1

--- TP3_Codigo/src/main/java/Punto3.java
+++ TP3_Codigo/src/main/java/Punto3.java
@@ -3,5 +3,52 @@ public class Punto3 {
     public static void main(String[] args) {
 
 
+        /*
+        Punto 1 :
+
+        Punto 1:
+        Implementar dos métodos adicionales a los fundamentales (push, pop, peek, isEmpty, isFull) en una estructura de pila
+        basada en un arreglo. Se sugiere, por ejemplo:
+        • Método que permita vaciar completamente la pila.
+        • Método que devuelva la cantidad de elementos presentes en la pila.
+
+    public int cantidadElementos(){
+        if (estaVacia()){
+            System.out.println("La pila está vacía");
+            return 0;
+        }
+        return tope +1;
+    }
+
+    public void vaciarPila(){
+        if (estaVacia()){
+            System.out.println("La pila está vacía");
+            return;
+        }
+        for (int i = 0 ; i <=tope; i++){
+            pila[i]= 0;
+        }
+        tope = -1;
+    }
+         */
+
+/*
+
+SE PRUEBAN ACÁ LOS 2 METODOS
+
+        Pila pila =  new Pila();
+        pila.mostrar();
+        pila.agregar(5);
+        pila.agregar(4);
+        pila.agregar(6);
+        pila.mostrar();
+        System.out.println(pila.cantidadElementos());
+        pila.vaciarPila();
+        pila.mostrar();
+
+ */
+
+
+
     }
 }
